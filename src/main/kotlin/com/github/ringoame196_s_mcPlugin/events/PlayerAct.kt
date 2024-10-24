5c6e8bb3406665293b60982f8de08945add6346d
りんご飴196 <kazumac41@gmail.com>
2024-10-24 22:42:27 +0900

    ✨  :sparkles: 認証プレイヤーの動作制限の内容変更

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/PlayerAct.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/PlayerAct.kt
@@ -1,6 +1,6 @@
 package com.github.ringoame196_s_mcPlugin.events
 
-import com.github.ringoame196_s_mcPlugin.managers.DataBaseManager
+import com.github.ringoame196_s_mcPlugin.managers.AuthPlayerManager
 import org.bukkit.ChatColor
 import org.bukkit.event.Cancellable
 import org.bukkit.event.EventHandler
@@ -13,27 +13,28 @@ import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.plugin.Plugin
 
 class PlayerAct(plugin: Plugin) : Listener {
-    private val dataBaseManager = DataBaseManager()
+    private val authPlayerManager = AuthPlayerManager(plugin)
 
     private fun actProhibiting(e: Cancellable) {
         if (e !is PlayerEvent) return
         val player = e.player
         val uuid = player.uniqueId.toString()
-        val message = "${ChatColor.RED}認証を完了させてください"
+        val message = "${ChatColor.RED}BOT認証をしてください \n/captcha auth <入力>"
 
-        if (isActProhibitingPlayer(uuid)) {
+        if (authPlayerManager.isActProhibitingPlayer(uuid)) {
             e.isCancelled = true
             player.sendMessage(message)
         }
     }
 
-    private fun isActProhibitingPlayer(uuid: String): Boolean {
-        return false
-    }
-
     @EventHandler
     fun onPlayerMove(e: PlayerMoveEvent) {
-        actProhibiting(e)
+        val to = e.to ?: return
+        val from = e.from
+
+        if (to.x != from.x || to.y != from.y || to.z != from.z) {
+            actProhibiting(e)
+        }
     }
 
     @EventHandler
