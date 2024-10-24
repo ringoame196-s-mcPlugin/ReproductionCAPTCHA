d7a0278873213574c89a442f4f3f08e86ec96488
りんご飴196 <kazumac41@gmail.com>
2024-10-24 22:46:26 +0900

    ✨  :sparkles: データベースを作成するように

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -3,15 +3,31 @@ package com.github.ringoame196_s_mcPlugin
 import com.github.ringoame196_s_mcPlugin.commands.Command
 import com.github.ringoame196_s_mcPlugin.commands.TabCompleter
 import com.github.ringoame196_s_mcPlugin.events.PlayerAct
+import com.github.ringoame196_s_mcPlugin.managers.DataBaseManager
+import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class Main : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
         val plugin = this
+        makeDatabaseFile(plugin)
+        saveResource("PlayerData.yml", false)
         server.pluginManager.registerEvents(PlayerAct(plugin), plugin)
         val command = getCommand("captcha")
-        command!!.setExecutor(Command())
+        command!!.setExecutor(Command(plugin))
         command.tabCompleter = TabCompleter()
+    }
+
+    private fun makeDatabaseFile(plugin: Plugin) {
+        val authDataFilePath = "${plugin.dataFolder}/${DataConst.FILEPATH}"
+        val authDataFile = File(authDataFilePath)
+        val dataBaseManager = DataBaseManager()
+        if (!authDataFile.exists()) {
+            val makeAuthKeyTableCommand =
+                "CREATE TABLE ${DataConst.TABLE_NAME}(${DataConst.MC_UUID} VARCHAR(36) NOT NULL, ${DataConst.AUTH_KEY} VARCHAR(5) NOT NULL);"
+            dataBaseManager.runSQLCommand(authDataFilePath, makeAuthKeyTableCommand, mutableListOf())
+        }
     }
 }
